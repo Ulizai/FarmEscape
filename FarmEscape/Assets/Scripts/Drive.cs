8384590b74ffc0e0d04fba20adf437816eb85e3c
Andrei Zavada <zavadaandrei@gmail.com>
2017-05-12 14:37:24 +0200

    Initial commit : a car driving around...in a crappy manner, a camera that zooms in and out, and LeanTween. A starting point with some of the core needed elements.

---
+++ FarmEscape/Assets/Scripts/Drive.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour {
+
+    [System.Serializable]
+    public class AxelInfo
+    {
+        public WheelCollider leftWheel;
+        public WheelCollider rightWheel;
+        public bool motor; 
+        public bool steering; 
+    }
+
+    public List<AxelInfo> axelInfos; 
+    public float maxMotorTorque; 
+    public float maxSteeringAngle; 
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxelInfo axleInfo in axelInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
+    }
+}
